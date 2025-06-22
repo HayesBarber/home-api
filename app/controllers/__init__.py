a5ce83f47d1f8069ba6370e524a161340452fc3a
Hayes Barber <you@example.com>
2025-06-21 22:14:59 -0400

    add router

--- app/controllers/__init__.py
+++ app/controllers/__init__.py
@@ -1,5 +1,7 @@
 from fastapi import APIRouter
 from app.controllers.discovery_controller import router as discovery_controller
+from app.controllers.lighting_controller import router as lighting_controller
 
 api_router = APIRouter()
 api_router.include_router(discovery_controller)
+api_router.include_router(lighting_controller)
