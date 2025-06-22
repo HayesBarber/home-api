10f4c78fc8d7c39b85cac566824138b5aa6fd480
Hayes Barber <you@example.com>
2025-06-21 22:26:50 -0400

    create device config controller

--- app/controllers/discovery_controller.py
+++ app/controllers/discovery_controller.py
@@ -13,15 +13,6 @@ def check_in_device(config: DeviceConfig):
 def trigger_discovery():
     return discovery_service.trigger_discovery()
 
-@router.get("/read")
-def read_all_devices():
-    return device_config_service.read_all_devices()
-
-@router.delete("/{device_name}", status_code=status.HTTP_204_NO_CONTENT)
-def delete_device(device_name: str):
-    device_config_service.delete_devcie(device_name)
-    return Response(status_code=status.HTTP_204_NO_CONTENT)
-
 @router.post("/check-offline")
 def check_for_offline_devices():
     return {
--- app/services/device_config_service.py
+++ app/services/device_config_service.py
@@ -1,8 +1,5 @@
 from app.utils.redis_client import redis_client, Namespace
-from datetime import datetime, timedelta
-from typing import List
 from app.models.device import DeviceConfig
-from app.utils import kasa_util, lifx_util
 
 def upsert_device(device_config: DeviceConfig):
     redis_client.set_model(Namespace.DEVICE_CONFIG, device_config.name, device_config)
