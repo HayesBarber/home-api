eb184673a4efd1c5024148a606a6ff14e06c89e6
Hayes Barber <you@example.com>
2025-06-21 22:29:45 -0400

    get device config

--- app/services/device_config_service.py
+++ app/services/device_config_service.py
@@ -1,5 +1,6 @@
 from app.utils.redis_client import redis_client, Namespace
 from app.models.device import DeviceConfig
+from typing import Optional
 
 def upsert_device(device_config: DeviceConfig):
     redis_client.set_model(Namespace.DEVICE_CONFIG, device_config.name, device_config)
@@ -10,3 +11,6 @@ def read_all_devices():
 
 def delete_devcie(name: str):
     redis_client.delete(Namespace.DEVICE_CONFIG, name)
+
+def get_device_config(name: str) -> Optional[DeviceConfig]:
+    return redis_client.get_model(Namespace.DEVICE_CONFIG, name, DeviceConfig)
