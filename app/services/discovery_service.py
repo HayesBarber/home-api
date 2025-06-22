a56a27dfa63abd3a7b65d4c5bc2ce3e758ad6c38
Hayes Barber <you@example.com>
2025-06-21 21:58:40 -0400

    rename files

--- app/services/discovery_service.py
+++ app/services/discovery_service.py
@@ -2,14 +2,14 @@ from app.utils.redis_client import redis_client, Namespace
 from datetime import datetime, timedelta
 from typing import List
 from app.models.device import DeviceConfig
-from app.utils import kasa, lifx
+from app.utils import kasa_util, lifx_util
 
 def upsert_device(device_config: DeviceConfig):
     redis_client.set_model(Namespace.DEVICE_CONFIG, device_config.name, device_config)
 
 def trigger_discovery():
-    lifx_devices = lifx.discover_lifx_devices()
-    kasa_devices = kasa.discover_kasa_devices()
+    lifx_devices = lifx_util.discover_lifx_devices()
+    kasa_devices = kasa_util.discover_kasa_devices()
     all_devices = lifx_devices + kasa_devices
 
     redis_client.set_all_models(Namespace.DEVICE_CONFIG, all_devices, "name")
--- app/utils/kasa_util.py
+++ app/utils/kasa_util.py
@@ -1,6 +1,6 @@
 import asyncio
-from kasa import Discover
-from app.models.device import DeviceConfig, DeviceType, PowerState
+from kasa import Discover, Device
+from app.models.device import DeviceConfig, DeviceType, PowerState, PowerAction
 from typing import List
 
 async def _discover_kasa_devices_async() -> List[DeviceConfig]:
@@ -27,3 +27,7 @@ async def _discover_kasa_devices_async() -> List[DeviceConfig]:
 
 def discover_kasa_devices() -> List[DeviceConfig]:
     return asyncio.run(_discover_kasa_devices_async())
+
+async def _control_kasa_device(config: DeviceConfig, action: PowerAction):
+    device = await Device.connect(host="172.16.2.109")
+    device.turn_off()
