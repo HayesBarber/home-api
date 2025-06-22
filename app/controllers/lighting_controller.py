83e0e05606c6cfbb53bc3bef9a9bb856138b65e9
Hayes Barber <you@example.com>
2025-06-21 22:31:30 -0400

    auto commit

--- app/controllers/lighting_controller.py
+++ app/controllers/lighting_controller.py
@@ -1,9 +1,9 @@
 from fastapi import APIRouter
 from app.services import lighting_service
-from app.models.device import DeviceConfig, PowerAction
+from app.models.device import PowerAction
 
 router = APIRouter(prefix="/lighting", tags=["Lighting"])
 
 @router.post("/set-state")
-def set_device_state(device: DeviceConfig, action: PowerAction):
-    return lighting_service.set_state(device, action)
+def set_device_state(device_name: str, action: PowerAction):
+    return lighting_service.set_state(device_name, action)
--- app/services/lighting_service.py
+++ app/services/lighting_service.py
@@ -1,6 +1,10 @@
 from app.models.device import DeviceConfig, PowerAction, DeviceType
 from app.utils import kasa_util
+from app.services import device_config_service
 
-def set_state(device: DeviceConfig, action: PowerAction):
+def set_state(name: str, action: PowerAction):
+    device = device_config_service.get_device_config(name)
+    if not device:
+        return
     if device.type == DeviceType.KASA:
         kasa_util.control_kasa_device(device, action)
