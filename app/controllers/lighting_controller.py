67cf36f25a01632f87c0b4459d62dcd98335c00c
Hayes Barber <you@example.com>
2025-06-23 21:41:39 -0400

    stub endpoint

--- app/controllers/lighting_controller.py
+++ app/controllers/lighting_controller.py
@@ -1,9 +1,13 @@
 from fastapi import APIRouter
 from app.services import lighting_service
-from app.models.device import PowerAction
+from app.models.device import PowerAction, Room
 
 router = APIRouter(prefix="/lighting", tags=["Lighting"])
 
 @router.post("/set-device-state")
 def set_device_state(device_name: str, action: PowerAction):
     return lighting_service.set_device_state(device_name, action)
+
+@router.post("/set-room-state")
+def set_room_state(room: Room, action: PowerAction):
+    pass
