a2b9c688c0cd1607e1fbf44435b44d82d4f998f5
Hayes Barber <you@example.com>
2025-06-24 20:19:04 -0400

    implement set room state

--- app/services/lighting_service.py
+++ app/services/lighting_service.py
@@ -1,5 +1,6 @@
 from app.models.device import PowerAction, DeviceType, Room
 from app.utils import kasa_util, lifx_util
+from app.utils.redis_client import redis_client, Namespace
 from app.services import device_config_service
 
 def set_device_state(name: str, action: PowerAction):
@@ -18,4 +19,20 @@ def set_device_state(name: str, action: PowerAction):
     return device
 
 def set_room_state(room: Room, action: PowerAction):
-    pass
+    devices = device_config_service.get_devices_of_room(room)
+    updated_devices = []
+
+    for device in devices:
+        match device.type:
+            case DeviceType.KASA:
+                new_state = kasa_util.control_kasa_device(device, action)
+            case DeviceType.LIFX:
+                new_state = lifx_util.control_lifx_device(device, action)
+            case _:
+                new_state = device.power_state
+
+        device.power_state = new_state
+        updated_devices.append(device)
+
+    redis_client.set_all_models(Namespace.DEVICE_CONFIG, updated_devices, "name")
+    return updated_devices
