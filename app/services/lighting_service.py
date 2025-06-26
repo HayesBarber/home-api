b47ed1414a1fc8c38b9e168c1dc5af5016c951b2
Hayes Barber <you@example.com>
2025-06-25 21:31:57 -0400

    set home state

--- app/services/lighting_service.py
+++ app/services/lighting_service.py
@@ -36,3 +36,22 @@ def set_room_state(room: Room, action: PowerAction):
 
     redis_client.set_all_models(Namespace.DEVICE_CONFIG, updated_devices, "name")
     return updated_devices
+
+def set_home_state(action: PowerAction):
+    devices = device_config_service.read_all_devices()
+    updated_devices = []
+
+    for device in devices:
+        match device.type:
+            case DeviceType.KASA:
+                new_state = kasa_util.control_kasa_device(device, action)
+            case DeviceType.LIFX:
+                new_state = lifx_util.control_lifx_device(device, action)
+            case _:
+                new_state = device.power_state
+
+        device.power_state = new_state
+        updated_devices.append(device)
+
+    redis_client.set_all_models(Namespace.DEVICE_CONFIG, updated_devices, "name")
+    return updated_devices
