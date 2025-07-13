7204d969c7fa3d9d44e5a400bdb75c4e8a796207
William Hayes Barber <barberhayes@gmail.com>
2025-07-13 19:59:15 -0400

    Change api routes (#19)

--- app/controllers/__init__.py
+++ app/controllers/__init__.py
@@ -1,9 +1,9 @@
 from fastapi import APIRouter
 from app.controllers.discovery_controller import router as discovery_controller
 from app.controllers.lighting_controller import router as lighting_controller
-from app.controllers.device_config_controller import router as config_controller
+from app.controllers.device_controller import router as device_controller
 
 api_router = APIRouter()
 api_router.include_router(discovery_controller)
 api_router.include_router(lighting_controller)
-api_router.include_router(config_controller)
+api_router.include_router(device_controller)
