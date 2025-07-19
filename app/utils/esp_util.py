6019369b61ce37cd805ab0b811891d9ec2cdfa8b
Hayes Barber <barberhayes@gmail.com>
2025-07-19 19:20:12 -0400

    add discover esp

--- app/utils/esp_util.py
+++ app/utils/esp_util.py
@@ -1,4 +1,7 @@
 import socket
 
-async def discover_esp_devices(passcode: str):
-    pass
+def discover_esp_devices(passcode: str, port: int):
+    with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as sock:
+        sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+        sock.bind(('', 8000))
+        sock.sendto(passcode.encode(), ('255.255.255.255', port))
