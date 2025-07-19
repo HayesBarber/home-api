6b9be6db24509737a9a4940159646b5b01e1d866
Hayes Barber <barberhayes@gmail.com>
2025-07-19 19:22:20 -0400

    make async

--- app/utils/esp_util.py
+++ app/utils/esp_util.py
@@ -1,7 +1,14 @@
 import socket
+import asyncio
 
-def discover_esp_devices(passcode: str, port: int):
-    with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as sock:
-        sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
-        sock.bind(('', 8000))
-        sock.sendto(passcode.encode(), ('255.255.255.255', port))
+async def discover_esp_devices(passcode: str, port: int):
+    loop = asyncio.get_running_loop()
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+    sock.bind(('', 8000))
+    sock.setblocking(False)
+
+    try:
+        await loop.sock_sendto(sock, passcode.encode(), ('255.255.255.255', port))
+    finally:
+        sock.close()
