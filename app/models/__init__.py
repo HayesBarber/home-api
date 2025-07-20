3ab40f1264ba794600a7bb33237b568cb696dc32
Hayes Barber <barberhayes@gmail.com>
2025-07-20 11:13:37 -0400

    new models

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,6 +1,6 @@
 from app.models.room import Room, get_room_from_string
 from app.models.device_type import DeviceType
-from app.models.theme import ApplyThemeRequest, THEME_CAPABLE_DEVICES
+from app.models.theme import ApplyThemeRequest, CreateThemeRequest, GetThemesResponse, THEME_CAPABLE_DEVICES
 from app.models.power_state import PowerState
 from app.models.power_action import PowerAction
 from app.models.device import DeviceConfig
@@ -10,6 +10,8 @@ __all__ = [
     "get_room_from_string",
     "DeviceType",
     "ApplyThemeRequest",
+    "CreateThemeRequest",
+    "GetThemesResponse",
     "THEME_CAPABLE_DEVICES",
     "PowerState",
     "PowerAction",
--- app/models/theme.py
+++ app/models/theme.py
@@ -4,4 +4,11 @@ from app.models.device_type import DeviceType
 class ApplyThemeRequest(BaseModel):
     colors: str
 
+class CreateThemeRequest(BaseModel):
+    name: str
+    colors: str
+
+class GetThemesResponse(BaseModel):
+    themes: dict
+
 THEME_CAPABLE_DEVICES = {DeviceType.LED_STRIP}
