033003d8ffc4e455757cc37d759c70a40a98c9e2
Hayes Barber <barberhayes@gmail.com>
2025-07-20 11:26:40 -0400

    auto commit

--- app/services/themes_service.py
+++ app/services/themes_service.py
@@ -1,12 +1,14 @@
 from app.models import CreateThemeRequest, DeleteThemeRequest, GetThemesResponse
 from app.utils.redis_client import redis_client, Namespace
 
-def save_theme(req: CreateThemeRequest) -> None:
+def save_theme(req: CreateThemeRequest) -> GetThemesResponse:
     redis_client.set(Namespace.THEME, req.name, req.colors)
+    return get_all_themes()
+
+def delete_theme(req: DeleteThemeRequest) -> GetThemesResponse:
+    redis_client.delete(Namespace.THEME, req.name)
+    return get_all_themes()
 
 def get_all_themes() -> GetThemesResponse:
     themes = redis_client.get_all(Namespace.THEME)
     return GetThemesResponse(themes=themes)
-
-def delete_theme(req: DeleteThemeRequest) -> None:
-    redis_client.delete(Namespace.THEME, req.name)
