a9b3bf1d3616d72b926bb2f8b4247bfcbfff8daa
Hayes Barber <barberhayes@gmail.com>
2025-07-20 11:30:23 -0400

    move method

--- app/services/themes_service.py
+++ app/services/themes_service.py
@@ -1,5 +1,8 @@
-from app.models import CreateThemeRequest, DeleteThemeRequest, GetThemesResponse
+from app.models import CreateThemeRequest, DeleteThemeRequest, GetThemesResponse, ApplyThemeRequest, THEME_CAPABLE_DEVICES, DeviceConfig, PowerState, DeviceType
 from app.utils.redis_client import redis_client, Namespace
+import asyncio
+from app.services import device_service
+from app.utils import led_strip_util
 
 def save_theme(req: CreateThemeRequest) -> GetThemesResponse:
     redis_client.set(Namespace.THEME, req.name, req.colors)
@@ -12,3 +15,23 @@ def delete_theme(req: DeleteThemeRequest) -> GetThemesResponse:
 def get_all_themes() -> GetThemesResponse:
     themes = redis_client.get_all(Namespace.THEME)
     return GetThemesResponse(themes=themes)
+
+async def set_theme(req: ApplyThemeRequest):
+    devices = device_service.read_all_devices()
+
+    async def _apply_theme(device: DeviceConfig) -> tuple[DeviceConfig, PowerState]:
+        match device.type:
+            case DeviceType.LED_STRIP:
+                new_state = await led_strip_util.set_led_theme(device, req.colors)
+            case _:
+                new_state = device.power_state
+        return device, new_state
+
+    theme_devices = [device for device in devices if device.type in THEME_CAPABLE_DEVICES]
+    results = await asyncio.gather(*[_apply_theme(device) for device in theme_devices])
+
+    for device, new_state in results:
+        device.power_state = new_state
+
+    redis_client.set_all_models(Namespace.DEVICE_CONFIG, devices, "name")
+    return devices
