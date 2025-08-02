644281f998835980c76e2e1d2389d93c32baafcf
Hayes Barber <barberhayes@gmail.com>
2025-08-02 17:18:54 -0400

    include user router

--- app/controllers/__init__.py
+++ app/controllers/__init__.py
@@ -3,9 +3,11 @@ from app.controllers.discovery_controller import router as discovery_controller
 from app.controllers.lighting_controller import router as lighting_controller
 from app.controllers.device_controller import router as device_controller
 from app.controllers.themes_controller import router as themes_controller
+from app.controllers.user_controller import router as users_controller
 
 api_router = APIRouter()
 api_router.include_router(discovery_controller)
 api_router.include_router(lighting_controller)
 api_router.include_router(device_controller)
 api_router.include_router(themes_controller)
+api_router.include_router(users_controller)
