3d5670886f2fa70330e79e8de6dd8229589cebb3
Hayes Barber <barberhayes@gmail.com>
2025-08-02 17:05:03 -0400

    call set and delete

--- app/services/user_service.py
+++ app/services/user_service.py
@@ -2,10 +2,10 @@ from app.utils.redis_client import redis_client, Namespace
 from app.models import CreateUserRequest, DeleteUserRequest
 
 def create_user(req: CreateUserRequest):
-    pass
+    redis_client.set(Namespace.USERS, req.name, req.public_key)
 
 def delete_user(req: DeleteUserRequest):
-    pass
+    redis_client.delete(Namespace.USERS, req.name)
 
 def get_all_users():
     pass
