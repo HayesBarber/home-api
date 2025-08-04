b10f5040dc3ca92fecfd9e8741ed5403a077a91e
Hayes Barber <barberhayes@gmail.com>
2025-08-04 19:19:44 -0400

    set response models

--- app/controllers/user_controller.py
+++ app/controllers/user_controller.py
@@ -1,17 +1,17 @@
 from fastapi import APIRouter
-from app.models import CreateUserRequest, DeleteUserRequest
+from app.models import CreateUserRequest, DeleteUserRequest, GetUsersResponse
 from app.services import user_service
 
 router = APIRouter(prefix="/users", tags=["Users"])
 
-@router.post("/")
+@router.post("/", response_model=GetUsersResponse)
 def create_user(req: CreateUserRequest):
     return user_service.create_user(req)
 
-@router.delete("/")
+@router.delete("/", response_model=GetUsersResponse)
 def delete_user(req: DeleteUserRequest):
     return user_service.delete_user(req)
 
-@router.get("/")
+@router.get("/", response_model=GetUsersResponse)
 def get_users():
     return user_service.get_all_users()
