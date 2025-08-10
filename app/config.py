bc6fc6e37bdd99b4376bed4d36b7e4d81e5e1003
Hayes Barber <barberhayes@gmail.com>
2025-08-10 11:02:57 -0400

    add config

---
+++ app/config.py
@@ -0,0 +1,13 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+from app.utils.logger import LOGGER
+
+class Settings(BaseSettings):
+    default_room: str = "Living Room"
+    model_config = SettingsConfigDict(
+        env_file=".env",
+        env_file_encoding="utf-8",
+    )
+
+settings = Settings()
+
+LOGGER.info(f"Loaded settings: {settings.model_dump_json()}")
