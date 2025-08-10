f88fcca4dfa47301fd04ab608844d0bdeacb29b6
Hayes Barber <barberhayes@gmail.com>
2025-08-10 11:27:34 -0400

    change get room from string

--- app/models/room.py
+++ app/models/room.py
@@ -1,5 +1,6 @@
 from enum import Enum
 from typing import Optional
+from app.services.device_service import get_devices_of_room
 
 class Room(str, Enum):
     BEDROOM = "bedroom"
@@ -7,7 +8,7 @@ class Room(str, Enum):
     UPSTAIRS = "upstairs"
 
 def get_room_from_string(name: str) -> Optional[Room]:
-    for room in Room:
-        if room.value == name:
-            return room
+    devices = get_devices_of_room(name)
+    if devices:
+        return name
     return None
