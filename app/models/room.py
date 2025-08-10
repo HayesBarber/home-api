5acdd8311263a73095038ca5cb2757b903ea0f04
Hayes Barber <barberhayes@gmail.com>
2025-08-10 11:47:44 -0400

    break circular import

--- app/models/room.py
+++ app/models/room.py
@@ -1,7 +1,7 @@
 from typing import Optional
-from app.services.device_service import get_devices_of_room
-
+    
 def get_room_from_string(name: str) -> Optional[str]:
+    from app.services.device_service import get_devices_of_room
     devices = get_devices_of_room(name)
     if devices:
         return name
