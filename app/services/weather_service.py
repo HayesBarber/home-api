65f62f3e244d0646711da48521e8b78397853cc5
Hayes Barber <barberhayes@gmail.com>
2025-08-23 11:21:37 -0400

    wrap in try catch

--- app/services/weather_service.py
+++ app/services/weather_service.py
@@ -1,5 +1,6 @@
 import time
 from app.config import settings
+from app.utils.logger import LOGGER
 import openmeteo_requests
 
 _last_fetch_time = 0
@@ -10,20 +11,24 @@ def get_current_temperature():
     current_time = time.time()
     if current_time - _last_fetch_time < 600:
         return _last_temperature
-    client = openmeteo_requests.Client()
-    params = {
-        "latitude": settings.latitude,
-        "longitude": settings.longitude,
-        "current": "temperature_2m",
-        "wind_speed_unit": "mph",
-        "temperature_unit": "fahrenheit",
-        "precipitation_unit": "inch",
-        "timezone": "America/New_York",
-    }
-    responses = client.weather_api(
-        "https://api.open-meteo.com/v1/forecast", params=params
-    )
-    response = responses[0]
-    _last_temperature = response.Current().Variables(0).Value()
-    _last_fetch_time = current_time
-    return _last_temperature
+    try:
+        client = openmeteo_requests.Client()
+        params = {
+            "latitude": settings.latitude,
+            "longitude": settings.longitude,
+            "current": "temperature_2m",
+            "wind_speed_unit": "mph",
+            "temperature_unit": "fahrenheit",
+            "precipitation_unit": "inch",
+            "timezone": "America/New_York",
+        }
+        responses = client.weather_api(
+            "https://api.open-meteo.com/v1/forecast", params=params
+        )
+        response = responses[0]
+        _last_temperature = response.Current().Variables(0).Value()
+        _last_fetch_time = current_time
+        return _last_temperature
+    except Exception as e:
+        LOGGER.error(f"Weather API error: {e}")
+        return _last_temperature
