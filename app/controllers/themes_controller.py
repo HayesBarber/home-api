627127c9a0165d802606d0d6c2359034a84a5811
Hayes Barber <barberhayes@gmail.com>
2025-08-23 18:10:39 -0400

    cleanup

--- app/controllers/themes_controller.py
+++ app/controllers/themes_controller.py
@@ -1,22 +1,21 @@
 from fastapi import APIRouter
-from app.models import ApplyThemeRequest, CreateThemeRequest, DeleteThemeRequest, GetThemesResponse, DeviceConfig, EffectedDevicesResponse
+from app.models import ApplyThemeRequest, CreateThemeRequest, DeleteThemeRequest, GetThemesResponse, EffectedDevicesResponse
 from app.services import themes_service
-from typing import List
 
 router = APIRouter(prefix="/themes", tags=["Themes"])
 
 @router.post("/apply", response_model=EffectedDevicesResponse)
-async def apply_theme(req: ApplyThemeRequest) -> List[DeviceConfig]:
+async def apply_theme(req: ApplyThemeRequest):
     return await themes_service.set_theme(req)
 
 @router.post("/", response_model=GetThemesResponse)
-def create_theme(req: CreateThemeRequest) -> GetThemesResponse:
+def create_theme(req: CreateThemeRequest):
     return themes_service.save_theme(req)
 
 @router.get("/", response_model=GetThemesResponse)
-def get_themes() -> GetThemesResponse:
+def get_themes():
     return themes_service.get_all_themes()
 
 @router.delete("/", response_model=GetThemesResponse)
-def delete_theme(req: DeleteThemeRequest) -> GetThemesResponse:
+def delete_theme(req: DeleteThemeRequest):
     return themes_service.delete_theme(req)
