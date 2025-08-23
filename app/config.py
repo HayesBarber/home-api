667b383d1098ca389b2db3328d5f0a2aa7b99155
Hayes Barber <barberhayes@gmail.com>
2025-08-23 11:04:49 -0400

    add lat and long to settings

--- app/config.py
+++ app/config.py
@@ -3,6 +3,8 @@ from app.utils.logger import LOGGER
 
 class Settings(BaseSettings):
     default_room: str = "Living Room"
+    latitude: float = 0.0
+    longitude: float = 0.0
     model_config = SettingsConfigDict(
         env_file=".env",
         env_file_encoding="utf-8",
