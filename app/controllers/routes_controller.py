2f4989d25c02a1e522251a3be925ec6debbf2e11
Hayes Barber <barberhayes@gmail.com>
2025-12-12 17:25:18 -0500

    add post and delete and fix typo

--- app/controllers/routes_controller.py
+++ app/controllers/routes_controller.py
@@ -1,4 +1,5 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Response, status
+from app.models.upstream import UpstreamMapping
 from app.services import routes_service
 from app.models import GetAllRoutesResponse
 
@@ -8,3 +9,15 @@ router = APIRouter(prefix="/routes", tags=["Routes"])
 @router.get("", response_model=GetAllRoutesResponse)
 async def get_routes():
     return routes_service.get_all_routes()
+
+
+@router.post("", status_code=status.HTTP_204_NO_CONTENT)
+def upsert_route(req: UpstreamMapping):
+    routes_service.upsert_route(req)
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
+
+
+@router.delete("", status_code=status.HTTP_204_NO_CONTENT)
+def delete_user(req: UpstreamMapping):
+    routes_service.delete_route(req)
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
