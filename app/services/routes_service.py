b62bab9c154695f6fd29ff2477daecd5d6103378
Hayes Barber <barberhayes@gmail.com>
2025-12-12 17:32:22 -0500

    set model

--- app/services/routes_service.py
+++ app/services/routes_service.py
@@ -3,7 +3,7 @@ from app.utils.redis_client import redis_client, Namespace
 
 
 def upsert_route(req: UpstreamMapping):
-    redis_client.set(Namespace.UPSTREAMS, req.prefix, req)
+    redis_client.set_model(Namespace.UPSTREAMS, req.prefix, req)
 
 
 def delete_route(req: UpstreamMapping):
